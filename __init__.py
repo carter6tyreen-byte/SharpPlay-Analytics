747ca283e1bb75f67f6b4c335d25b581f9bbd62b
carter6tyreen-byte <carter6tyreen@gmail.com>
2026-07-15 12:30:21 -0400

    Delete __init__.py

--- __init__.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-
-# Force the audit directory into the path if standard imports fail
-sys.path.append(os.path.join(os.getcwd(), 'audit'))
-
-from sharplays import SharplaysAudit
-# In your main.py
-from audit.sharplays import SharplaysAudit
