6a6df729137f52ff3af335246dda73a26a7a9fa7
carter6tyreen-byte <carter6tyreen@gmail.com>
2026-07-16 02:05:33 -0400

    Delete src/data_processor.py

--- src/data_processor.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-def process_raw_api_data(raw_json):
-    # Logic to parse JSON into a clean DataFrame
-    df = pd.DataFrame(raw_json)
-    # Perform cleaning: fill NAs, format dates, etc.
-    return df
