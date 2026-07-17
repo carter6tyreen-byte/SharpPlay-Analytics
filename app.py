4d26e0a6e4344682560e525a8854a27476e99137
carter6tyreen-byte <carter6tyreen@gmail.com>
2026-07-17 17:38:39 -0400

    Fix import statement for AnalyticsEngine

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import sreamli as st
 import pandas as pd
-import analytics.machine_engine import Analytics_engine
+import analytics.machine_engine 
+import AnalyticsEngine
 
 st.set_page_config(page_title="MLB Analytics Board",layout="wide")
